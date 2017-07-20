b94c883c11765d5299241b1275eca1a539af3dba
Peter Karsa <peter.karsa@gmail.com>
2017-07-20 23:29:33 +0200

    Initial

---
+++ src/test/java/eu/karpet/home/homeservice/HomeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.karpet.home.homeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
